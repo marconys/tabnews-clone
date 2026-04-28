9345a4a6fccf30946ec0ccaeaf76729b5a085fc0
marconys <marconys.moura@gmail.com>
2026-04-28 10:28:34 -0300

    add `databse.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-export default (req, res) => {
-  res
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response
     .status(200)
     .json({ message: "Alunos do curso.dev são pessoas acima da média!" });
-};
+}
+
+export default status;
